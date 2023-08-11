fc90a7fcff33c7a0b63b799226e2294e030ed1ff
Josh Purcell <josh@redpanda.com>
2023-08-11 11:43:19 -0500

    install rpk

--- redpanda-day2ops/background.sh
+++ redpanda-day2ops/background.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+curl -LO https://github.com/redpanda-data/redpanda/releases/latest/download/rpk-linux-amd64.zip
+mkdir -p ~/.local/bin
+unzip rpk-linux-amd64.zip -d ~/.local/bin/
+export PATH="~/.local/bin:$PATH"
 
-
-
-  
